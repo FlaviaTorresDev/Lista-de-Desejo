b0fc9f39b98d3e31c7e2bf3ddd134f643b81f7c6
FlaviaTorresDev <flaviatorresdev@gmail.com>
2024-07-20 23:42:07 -0300

    criando o projeto

---
+++ listaDesejo/src/main/java/com/flaviadev/listaDesejo/ListaDesejoApplication.java
@@ -0,0 +1,13 @@
+package com.flaviadev.listaDesejo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaDesejoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaDesejoApplication.class, args);
+	}
+
+}
---
+++ listaDesejo/src/test/java/com/flaviadev/listaDesejo/ListaDesejoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flaviadev.listaDesejo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaDesejoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
