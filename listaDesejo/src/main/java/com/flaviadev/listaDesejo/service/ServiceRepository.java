896c6555706d330692d902d6b06919ca21c590ee
FlaviaTorresDev <flaviatorresdev@gmail.com>
2024-07-21 00:03:11 -0300

    metodo de creat no service

---
+++ listaDesejo/src/main/java/com/flaviadev/listaDesejo/service/ServiceRepository.java
@@ -0,0 +1,19 @@
+package com.flaviadev.listaDesejo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.flaviadev.listaDesejo.model.Desejo;
+import com.flaviadev.listaDesejo.repository.DesejoRepository;
+
+@Service
+public class ServiceRepository {
+	
+	@Autowired
+	private DesejoRepository desejoRepository;
+	
+	public Desejo creatDesejo(Desejo desejo) {
+		return desejoRepository.save(desejo);
+	}
+
+}
