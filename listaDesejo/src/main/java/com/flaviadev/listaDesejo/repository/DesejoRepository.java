50319bfbc59d565f73c485c7d3a3d6ee67a0c038
FlaviaTorresDev <flaviatorresdev@gmail.com>
2024-07-20 23:54:24 -0300

    add interface repository

---
+++ listaDesejo/src/main/java/com/flaviadev/listaDesejo/repository/DesejoRepository.java
@@ -0,0 +1,9 @@
+package com.flaviadev.listaDesejo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.flaviadev.listaDesejo.model.Desejo;
+
+public interface DesejoRepository  extends JpaRepository<Desejo, Integer>{
+
+}
