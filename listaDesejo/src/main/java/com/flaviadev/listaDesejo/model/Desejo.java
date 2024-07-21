f1e7c3cb16bcb7096f63fe53165e24ab3235feb9
FlaviaTorresDev <flaviatorresdev@gmail.com>
2024-07-20 23:52:11 -0300

    add campos na tabela

--- listaDesejo/src/main/java/com/flaviadev/listaDesejo/model/Desejo.java
+++ listaDesejo/src/main/java/com/flaviadev/listaDesejo/model/Desejo.java
@@ -1,8 +1,34 @@
 package com.flaviadev.listaDesejo.model;
 
+
+import java.time.LocalDate;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Desejo {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String categoriaDesejo;
+	
+	private String tituloDesejo;
+	
+	private String descricaoDesejo;
+	
+	private LocalDate dataDesejo;
 
 }
