84e98733e365a78f7f35b732fa50f7b60ff9c684
FlaviaTorresDev <flaviatorresdev@gmail.com>
2024-07-22 22:41:58 -0300

    alterando o JsonFormat

--- listaDesejo/src/main/java/com/flaviadev/listaDesejo/model/Desejo.java
+++ listaDesejo/src/main/java/com/flaviadev/listaDesejo/model/Desejo.java
@@ -3,6 +3,8 @@ package com.flaviadev.listaDesejo.model;
 
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -29,6 +31,7 @@ public class Desejo {
 	
 	private String descricaoDesejo;
 	
+	@JsonFormat(pattern = "dd-MM-yyyy")
 	private LocalDate dataDesejo;
 
 }
