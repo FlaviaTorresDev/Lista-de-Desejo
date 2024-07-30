3725bc82ce8f49c8a5a4ece58f58f034d47c367f
FlaviaTorresDev <flaviatorresdev@gmail.com>
2024-07-29 22:34:12 -0300

    add controllers (@DeleteMapping)

--- listaDesejo/src/main/java/com/flaviadev/listaDesejo/controller/ControllerDesejo.java
+++ listaDesejo/src/main/java/com/flaviadev/listaDesejo/controller/ControllerDesejo.java
@@ -1,9 +1,12 @@
 package com.flaviadev.listaDesejo.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,4 +31,15 @@ public class ControllerDesejo {
 	public List<Desejo> listDesejo(){
 		return serviceDesejo.listDesejo();
 	}
+	
+	@GetMapping("/{id}")
+	public Optional<Desejo> listDesejoById(@PathVariable Long id){
+		return serviceDesejo.listDesejoById(id);
+	}
+	
+	@DeleteMapping("/{id}")
+	public void deleteDesejoById(@PathVariable Long id){
+		serviceDesejo.deleteDesejoById(id);
+	}
+	
 }
